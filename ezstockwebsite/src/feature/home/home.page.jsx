b770d86dc60a4f94ec78a3ed0335a15d759cba9e
khoa3a <khoa3a@gmail.com>
2021-03-26 18:02:15 +0700

    add sandbox page (test PayPal integration)

--- ezstockwebsite/src/feature/home/home.page.jsx
+++ ezstockwebsite/src/feature/home/home.page.jsx
@@ -8,8 +8,19 @@ class HomePage extends React.Component {
     render() {
         return (
             <div className="col-md-6 col-md-offset-3">    
-                Welcome to the Home Page!    
-            </div>
+                Welcome to the SandBox Page!    
+
+                <form action="https://www.sandbox.paypal.com/cgi-bin/webscr" method="post">
+
+                    <input type="hidden" name="cmd" value="_xclick" />
+                    <input type="hidden" name="business" value="sb-9jd7n3606747@business.example.com" />
+
+                    <input type="hidden" name="item_name" value="Heineken Beer 1" />
+                    <input type="hidden" name="amount" value="10.00" /> 
+                    <input type="submit" value="Paypal" />
+
+                </form>
+            </div>            
         );
     }
 }
